137c88c5e0a8ca51302b33ce3a5de194cd811e86
sergiuchilat <chilatsergiu@gmail.com>
2022-01-19 15:51:32 +0200

    Exemplu 1. Conectarea JS in pagina HTML

---
+++ exemple/1. JS Conectare/app.js
@@ -0,0 +1,7 @@
+document.body.style.backgroundColor = "red";
+document.body.innerHTML = "I am a JS code";
+
+setTimeout(() => {
+    document.body.style.backgroundColor = "green";
+    document.body.innerHTML = "It's time to learn Frontend";
+}, 3000);
