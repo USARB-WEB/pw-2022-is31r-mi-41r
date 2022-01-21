587482a66d54a1e6bf4ca640a97903c309a8f644
sergiuchilat <chilatsergiu@gmail.com>
2022-01-21 15:34:38 +0200

    6. Obiecte

---
+++ exemple/6. Obiecte/app.js
@@ -0,0 +1,15 @@
+const person = {
+    name: "Ion Creanga",
+    age: 12,
+    isStudent: true
+};
+
+console.log(person);
+
+console.log(person.name);
+console.log(person.age);
+console.log(person.isStudent);
+
+person.age = 33;
+
+console.log(person);
